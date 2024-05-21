c410541d8b77bf6dc007615b1606fb26d0e3c280
padmavathi <padmavathichilamala@gmail.com>
2024-05-21 22:12:40 +0530

    Added Order controller implementation

---
+++ RepositoryLayer/RInterfaces/IOrder.cs
@@ -0,0 +1,15 @@
+﻿using ModelLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.RInterfaces
+{
+    public interface IOrder
+    {
+        public Task<bool> AddOrder(OrderEntity re_var);
+        public Task<IEnumerable<object>> GetOrder();
+    }
+}
--- RepositoryLayer/obj/Debug/net8.0/RepositoryLayer.AssemblyInfo.cs
+++ RepositoryLayer/obj/Debug/net8.0/RepositoryLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee55ebe9e77ac2f0b58dc0cf9f0246dea772d4b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe86887f3580332f43890e2b145783a15430838")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
